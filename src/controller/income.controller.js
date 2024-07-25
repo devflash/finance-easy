3c5e5edb673a337c76ec95dbf81216fb4f6d5512
Mrugesh Wadagbalkar <mwadagbalkar@eisgroup.com>
2024-07-25 19:24:51 +0100

    Added income update and delete routes and controllers

--- src/controller/income.controller.js
+++ src/controller/income.controller.js
@@ -46,3 +46,39 @@ export const getIncomes = async (req, resp, next) => {
         next(error)
     }
 }
+
+export const updateIncome = async (req, res, next) => {
+    try {
+        const {incomeId} = req.params
+        const {source, amount, incomeDate, depositType, description, category} = req.body
+
+        const requiredFields = {incomeId, source, amount, incomeDate, depositType, description, category}
+        validateMandatory(requiredFields)
+        const income = await Income.updateOne(
+            {_id: incomeId},
+            {incomeId, source, amount, incomeDate, depositType, description, category}
+        )
+
+        const updatedIncome = await Income.findById(income._id).select('-userId')
+        res.status(200).json(updatedIncome)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteIncome = async (req, res, next) => {
+    try {
+        const {incomeId} = req.params
+
+        const requiredFields = {incomeId}
+        validateMandatory(requiredFields)
+
+        await Income.deleteOne({_id: incomeId})
+
+        res.status(200).json({
+            msg: `Income ${incomeId} is deleted from the records successfuly`
+        })
+    } catch (error) {
+        next(error)
+    }
+}
--- src/routes/income.routes.js
+++ src/routes/income.routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
-import {createIncome, getIncomeById, getIncomes} from '../controller/income.controller.js'
+import {createIncome, getIncomeById, getIncomes, updateIncome, deleteIncome} from '../controller/income.controller.js'
 import {auth} from '../middlewares/auth.js'
 const router = Router()
 
 router.post('/create', auth, createIncome)
 router.get('/all', auth, getIncomes)
 router.get('/:incomeId', auth, getIncomeById)
+router.put('/:incomeId', updateIncome)
+router.delete('/:incomeId', deleteIncome)
 
 export default router
